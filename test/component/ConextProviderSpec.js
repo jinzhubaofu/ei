82e7e9c7ee1ce45a06f80b547a755a3b745e7eb8
lupengyu <lupengyu@baidu.com>
2015-07-31 11:00:20 +0800

    添加ContextProvider的测试用例

---
+++ test/component/ConextProviderSpec.js
@@ -0,0 +1,53 @@
+/**
+ * @file context provider spec
+ * @author Leon(leon@outlook.com)
+ */
+
+var ContextProvider = require('../../lib/component/ConextProvider.js');
+var React = require('react');
+
+describe('ContextProvider', function () {
+
+    it('is a React Component', function () {
+
+        var store = {};
+
+        function dispatch() {
+
+        }
+
+        var context = {
+            store: store,
+            dispatch: dispatch
+        };
+
+        var View = React.createClass({
+
+            render: function () {
+                return React.createElement('div');
+            }
+
+        });
+
+        var provider = React.createElement(
+            ContextProvider,
+            {
+                ei: context
+            },
+            function (state, dispatch1) {
+
+                expect(state).toBe(store);
+                expect(dispatch1).toBe(dispatch);
+
+                return React.createElement(View);
+            }
+        );
+
+        var string = React.renderToStaticMarkup(provider);
+
+
+        expect(string).toBe('<div></div>');
+
+    });
+
+});
