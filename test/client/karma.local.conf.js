e6766fcf582ea8701b9dd1c7f9c3c3ace92a9d6d
lupengyu <lupengyu@baidu.com>
2016-12-08 15:36:46 +0800

    switch test framework from browserfy to karma webpack

--- test/client/karma.local.conf.js
+++ test/client/karma.local.conf.js
@@ -11,7 +11,7 @@ module.exports = function (config) {
             {},
             karmaConfig,
             {
-                singleRun: false
+                singleRun: true
             }
         )
     );
